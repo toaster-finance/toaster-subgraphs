293580e61b1e328cda16ccecdd2d37a61d98d018
suha jin <89185836+djm07073@users.noreply.github.com>
2024-05-10 13:04:08 +0900

    [Fix] Debug: initAmount 0

--- src/projects/aave-v3/type.ts
+++ src/projects/aave-v3/type.ts
@@ -4,7 +4,34 @@ import { PoolDataProvider } from "../../../generated/Pool/PoolDataProvider";
 import { getContextAddress } from "../../common/helpers/contextHelper";
 import { Position } from "../../../generated/schema";
 
-export class ReserveUserData {
+export class InvestUserData {
+  helper: AaveV3Helper;
+  underlyingAmount: BigInt;
+  stableDebt: BigInt;
+  variavbleDebt: BigInt;
+  constructor(owner: Address, underlying: Address, amount: BigInt) {
+    const pool = dataSource.address();
+    const poolDataProvider = PoolDataProvider.bind(
+      getContextAddress("dataProvider")
+    );
+    this.helper = new AaveV3Helper(pool, underlying.toHexString());
+    const posId = this.helper.getPositionId(owner, "");
+    const position = Position.load(posId);
+
+    if (!position) {
+      this.underlyingAmount = amount;
+      this.stableDebt = BigInt.zero();
+      this.variavbleDebt = BigInt.zero();
+    } else {
+      const userData = poolDataProvider.getUserReserveData(underlying, owner);
+      this.underlyingAmount = userData.getCurrentATokenBalance();
+      this.stableDebt = userData.getCurrentStableDebt();
+      this.variavbleDebt = userData.getCurrentVariableDebt();
+    }
+  }
+}
+
+export class BorrowUserData {
   helper: AaveV3Helper;
   underlyingAmount: BigInt;
   stableDebt: BigInt;
@@ -16,17 +43,10 @@ export class ReserveUserData {
     );
     this.helper = new AaveV3Helper(pool, underlying.toHexString());
     const posId = this.helper.getPositionId(owner, "");
-    const position = Position.load(posId);
 
-    if (!position) {
-      this.underlyingAmount = BigInt.zero();
-      this.stableDebt = BigInt.zero();
-      this.variavbleDebt = BigInt.zero();
-    } else {
-      const userData = poolDataProvider.getUserReserveData(underlying, owner);
-      this.underlyingAmount = userData.getCurrentATokenBalance();
-      this.stableDebt = userData.getCurrentStableDebt();
-      this.variavbleDebt = userData.getCurrentVariableDebt();
-    }
+    const userData = poolDataProvider.getUserReserveData(underlying, owner);
+    this.underlyingAmount = userData.getCurrentATokenBalance();
+    this.stableDebt = userData.getCurrentStableDebt();
+    this.variavbleDebt = userData.getCurrentVariableDebt();
   }
 }
